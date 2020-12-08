438eed9635f413b3a5c641862491cdb58e0dc316
midnight-trigger <junya4318@gmail.com>
2020-12-08 12:19:13 +0800

    create: routing

--- api/routes.go
+++ api/routes.go
@@ -6,3 +6,7 @@ type UserRoutes struct {
 	PostSigninUser void `method:"POST" path:"api/v1/users/signin"`
 	PostUser       void `method:"POST" path:"api/v1/users"`
 }
+
+type TodoRoutes struct {
+	PostTodo void `method:"POST" path:"api/v1/todos" auth:"true"`
+}
