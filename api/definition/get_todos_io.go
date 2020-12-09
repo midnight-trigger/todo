b173c70b785723eb5e101f8e23d966b54f9aada4
midnight-trigger <junya4318@gmail.com>
2020-12-09 22:01:39 +0800

    create: definition

---
+++ api/definition/get_todos_io.go
@@ -0,0 +1,43 @@
+package definition
+
+import (
+	"github.com/labstack/echo"
+	"github.com/midnight-trigger/todo/logger"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+type GetTodosParam struct {
+	Limit  int    `query:"limit" validate:"required"`
+	Offset int    `query:"offset" validate:"required"`
+	Sort   string `query:"sort" validate:"required,oneof=DESC ASC"`
+	Title  string `query:"title" validate:"lte=255"`
+	Body   string `query:"body"`
+	Status string `query:"status" validate:"required,oneof=todo progress finished"`
+}
+
+type GetTodosResponse struct {
+	Id        int64  `json:"id"`
+	UserId    string `json:"user_id"`
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	Status    string `json:"status"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+func CreateGetTodosParam(ctx echo.Context) (params *GetTodosParam, err error) {
+	params = new(GetTodosParam)
+	if err = ctx.Bind(params); err != nil {
+		logger.L.Error(err)
+		return
+	}
+	if params.Limit == 0 {
+		params.Limit = viper.GetInt("todos.listLimit")
+	}
+	if message, ok := Validator(params); !ok {
+		err = errors.New(message)
+		logger.L.Error(message)
+	}
+	return
+}
